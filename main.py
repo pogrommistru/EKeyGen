3aaea7ab96e4db27038bd432a69eff3dbf2dc3ba
Anton <mezhmidinov@gmail.com>
2024-10-31 10:50:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -364,6 +364,8 @@ def main(disable_exit=False):
                       activate_products = '\n🔓 Активирует: *ESET Smart Security Premium, ESET HOME Security Premium, ESET MOBILE SECURITY*'
                     if args['small_business_key']:
                      activate_products = '\n🔓 Активирует: *ESET Small Business Security, ESET Cyber Security \(MacOS\), ESET Mobile Security, ESET Smart TV Security, ESET Safe Server*'
+                    if args['vpn-codes']:
+                     activate_products = '\n🔓 Активирует: *ESET VPN*'
                     bot.send_message(-1001233475775, output_line + activate_products + "\n\n[⚡️Boost\!](https://t\.me/boost/esetnod32keyzz)", disable_web_page_preview=True)
                     if args['vpn_codes']:
                         EV_obj = EV(email_obj, driver, ER_obj.window_handle)
